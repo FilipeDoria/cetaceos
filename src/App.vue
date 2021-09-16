298eded5c271941b3f58872885dddbdf842d8b99
FilipeDoria <filipe0doria@gmail.com>
2021-09-16 22:14:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+const { t } = useI18n()
+
+useHead({
+  title: t('general.title'),
+  meta: [
+    { name: 'description', content: t('intro.desc') },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
---
+++ src/pages/records.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useUserStore } from '~/stores/user'
+
+const user = useUserStore()
+const name = ref(user.savedName)
+
+const router = useRouter()
+const go = () => {
+  if (name.value)
+    router.push(`/hi/${encodeURIComponent(name.value)}`)
+}
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div>
+    <p class="text-4xl">
+      <carbon-campsite class="inline-block" />
+    </p>
+    <p>
+      {{ t('general.title') }}
+    </p>
+    <p>
+      <em class="text-sm opacity-75">{{ t('intro.desc') }}</em>
+    </p>
+
+    <div class="py-4" />
+
+    <p>Records will appear on this page</p>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
