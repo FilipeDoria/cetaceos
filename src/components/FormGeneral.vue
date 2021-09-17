7c6ff40063a909ea104aac505b57e121d5971b08
FilipeDoria <filipe0doria@gmail.com>
2021-09-17 18:50:55 +0100

    Form General template example

--- src/components/FormGeneral.vue
+++ src/components/FormGeneral.vue
@@ -1,3 +1,69 @@
+<script setup lang="js">
+export default {
+  data() {
+    return {
+      formValues: {
+        company: '',
+        ship: '',
+        especie: '',
+        date: '',
+        time: '',
+        latitude: '',
+        longitude: '',
+        misto: false,
+        total_individuos: '',
+        crias: '',
+        comportamento: '',
+        reacao: '',
+        estado_mar: '',
+        observacoes: '',
+      },
+    }
+  },
+  methods: {
+    handleSubmit() {
+      // to save form items on local storage to userData variable
+      localStorage.setItem('userData', JSON.stringify(this.formValues))
+      console.log(`Form values saved on localStorage: ${localStorage.getItem('userData')}`)
+    },
+    loadInitialData(form) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        form.latitude = pos.coords.latitude.toFixed(5)
+        form.longitude = pos.coords.longitude.toFixed(5)
+      })
+    },
+
+  },
+}
+</script>
+
 <template>
-  <p>Hello from the component</p>
+  <div>
+    <h2>Ficha de registos de observações de cetáceos</h2>
+    <p>Registe o máximo de informação possível.</p>
+    <input
+      v-model="formValues.company"
+      name="company"
+      label="Empresa"
+      validation="required"
+      placeholder="ACME"
+    /><br />
+    <input v-model="formValues.latitude" name="latitude" label="Latitude" validation="required" /><br />
+    <input
+      v-model="formValues.longitude"
+      name="longitude"
+      label="Longitude"
+      validation="required"
+    /><br />
+    <button
+      class="m-3 text-sm btn"
+      @click="loadInitialData(formValues)"
+    >
+      Get Location
+    </button>
+    <h3>Values:</h3>
+    {{ formValues.company }}
+    {{ formValues.latitude }}
+    {{ formValues.longitude }}
+  </div>
 </template>
